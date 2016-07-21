1ce39741886cdce69e3801a1d0afb25c39a8b844
Matt Bachmann <bachmann.matt@gmail.com>
2016-07-20 23:14:33 -0400

    Add repr and str to our token model

--- fitbit/models.py
+++ fitbit/models.py
@@ -5,3 +5,9 @@ from django.db import models
 class Token(models.Model):
     fitbit_id = models.CharField(max_length=50)
     refresh_token = models.CharField(max_length=120)
+
+    def __repr__(self):
+        return '<Token %s>' % self.fitbit_id
+
+    def __str__(self):
+        return self.fitbit_id
